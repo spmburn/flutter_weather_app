ba3a764463c89c8ee4438f7251297b9ff8f4f2ca
spmburn <andriifiialko@gmail.com>
2023-06-27 17:05:40 -0500

    remove device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,3 @@
-import 'dart:io';
-import 'dart:ui';
-
-import 'package:device_preview_screenshot/device_preview_screenshot.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -10,26 +5,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'config/router/app_router.dart';
 import 'config/theme/app_theme.dart';
 
-void main() async{
+void main() async {
   await dotenv.load(fileName: ".env");
-  if (Platform.isAndroid || Platform.isIOS) {
-    runApp(
-      const ProviderScope(child: MyApp()),
-    );
-  }
-
-  // with extra config for device preview
   runApp(
-    DevicePreview(
-      enabled: !kReleaseMode,
-      tools: [
-        ...DevicePreview.defaultTools,
-        DevicePreviewScreenshot(
-          onScreenshot: screenshotAsFiles(Directory('/home/saul/Pictures/')),
-        ),
-      ],
-      builder: (context) => const ProviderScope(child: MyAppPreview()),
-    ),
+    const ProviderScope(child: MyApp()),
   );
 }
 
@@ -43,24 +22,6 @@ class MyApp extends StatelessWidget {
       routerConfig: appRouter,
       theme: AppTheme.getTheme(),
       title: 'Material App',
-    );
-  }
-}
-
-class MyAppPreview extends StatelessWidget {
-  const MyAppPreview({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      scrollBehavior:
-          const MaterialScrollBehavior().copyWith(dragDevices: {PointerDeviceKind.mouse}),
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
-      debugShowCheckedModeBanner: false,
-      routerConfig: appRouter,
-      title: 'Material App',
-      theme: AppTheme.getTheme(),
     );
   }
 }
